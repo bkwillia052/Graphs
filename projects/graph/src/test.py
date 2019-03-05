f2bc77470ca3aeaa2b6377ee277663746aaeb752
bkwillia052 <bkwillia052@gmail.com>
2019-03-04 19:27:40 -0600

    fully functional iteration of part 3.5 complete; might go back and optimize later

--- projects/graph/src/test.py
+++ projects/graph/src/test.py
@@ -86,27 +86,77 @@ class Graph:
                     print('bft neighbor', neighbor)
                     q.enqueue(neighbor)
 
+    def dftr(self, start, stack, visited):
+        print(
+            f"FIRST: node:{start},neighbors {self.vertices[start]} visited: {visited} \n Stack:{stack.stack}")
+
+        if visited == None:
+            print(f"VISITED NONE: node:{start}, visited: {visited}")
+            return
+        if start in visited:
+            print('start in visited')
+            if not stack.stack:
+                print('no stack')
+                return visited
+
+            node = stack.pop()
+            print(
+                f"node: {node} neighbors {self.vertices[start]} \n stack: {stack.stack}")
+            for neighbor in self.vertices[start]:
+                if neighbor not in visited:
+                    stack.push(neighbor)
+                    visited = self.dftr(neighbor, stack, visited)
+                    return visited
+            visited = self.dftr(node, stack, visited)
+            return visited
+
+        stack.push(start)
+        visited.append(start)
+        for neighbor in self.vertices[start]:
+            print(
+                f"POST-PUSH -- node:{start}: neighbors {self.vertices[start]} nextNode: {neighbor}, visited: {visited} \n Stack:{stack.stack}")
+            visited = self.dftr(neighbor, stack, visited)
+            print("other", visited)
+
     def dft(self, starting_vertex_id):
         print('DFT')
-        # Create an empty stack
+
         s = Stack()
-        # Create an empty set of visited vertices
-        visited = set()
-        # Put the starting vertex in our Stack
-        s.push(starting_vertex_id)
-        # While the stack is not empty....
-        while s.size() > 0:
-            # Pop the top node from the stack
-            print('before', s.stack)
+
+        visited = []
+
+        self.dftr(starting_vertex_id, s, visited)
+        """ s.push(starting_vertex_id) """
+
+        """ while s.size() > 0:
             v = s.pop()
-            print('after', s.stack)
-            # If that node has not been visted...
             if v not in visited:
-                # Mark it as visited
-                print('not visited', v)
                 visited.add(v)
-                print('visited arr: ', visited)
-                # Then, put all of it's children into the stack
                 for neighbor in self.vertices[v]:
-                    print('neighbors: ', self.vertices[v], neighbor)
-                    s.push(neighbor)
+                    print('neighbor: ', neighbor)
+                    s.push(neighbor) """
+        print("OUTER VISITED", visited)
+        print("OUTER stack", s.stack)
+        return visited
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_directed_edge('5', '3')
+graph.add_directed_edge('6', '3')
+graph.add_directed_edge('7', '1')
+graph.add_directed_edge('4', '7')
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('7', '6')
+graph.add_directed_edge('2', '4')
+graph.add_directed_edge('3', '5')
+graph.add_directed_edge('2', '3')
+graph.add_directed_edge('4', '6')
+print(graph.vertices)
+graph.dft('1')
